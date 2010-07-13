cc17a0b5dab8649fb68acd853bc97fe098d79912
aj00200 <aj0020020@live.com>
2010-07-13 15:00:45 -0500

    Started switch to Asynchat

--- q.py
+++ q.py
@@ -1,3 +1,6 @@
+import config
+import asyncore
+import socket
 class queue_class():
     def __init__(self):
         self.queue=[]
@@ -27,3 +30,25 @@ class queue_class():
     def raw(self,data):
         self.queue.append(data)
 queue=queue_class() 
+import asynchat
+class connection(asyncore.dispatcher):
+    def __init__(self):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((config.network,config.port))
+    def handle_connect(self):
+        self.buffer='NICK %s\r\n'%config.nick
+        self.buffer+='USER %s %s %s :%s\r\n'%(config.nick,config.nick,config.nick,config.nick)
+    def handle_close(self):
+        self.close()
+    def handle_read(self):
+        print self.recv(512)
+    def writable(self):
+        return (len(self.buffer) > 0)
+    def handle_write(self):
+        sent = self.send(self.buffer)
+        self.buffer = self.buffer[sent:]
+
+c = connection
+while 1:
+    asyncore.loop()
