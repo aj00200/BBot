6cc291ba106ed5d327846f9166afa7bd57db72df
Austin H <aj00200@AJ00200u.(none)>
2010-07-15 18:20:09 -0400

    Prevent overflow in mathbot()

--- modules/mathbot.py
+++ modules/mathbot.py
@@ -38,6 +38,9 @@ class mathbot():
         if self.ldata.find(':?math help')!=-1:
             q.queue.append((channel,nick+' : +, -, *, /, %, sqrt, pow, ceil, floor, log, asin, acos, atan, atan2, sin, cos, tan'))
         elif self.ldata.find(':?math ')!=-1:
+            if self.ldata.find('**')!=-1:
+                queue.append((channel,'Please use pow instead of **'))
+                self.e=0
             self.e=self.ldata[self.ldata.find('?math ')+6:].strip('\r\n')
             self.e=self.e.replace('!pi','3.1415926535897931')
             self.e=self.e.replace('!e',str(math.e))
