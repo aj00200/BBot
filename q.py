d87edb3d00ade3f5bc90a50911f756a3db5c88e4
Austin H <aj00200@AJ00200u.(none)>
2010-07-18 21:57:02 -0400

    Fixed blocking with connection class

--- q.py
+++ q.py
@@ -7,8 +7,33 @@ import asyncore
 import socket
 import api
 import re
-
-class connection(asynchat.async_chat):
+class queue_class():
+    def __init__(self):
+        self.queue=[]
+    def append(self,data):
+        self.go('PRIVMSG '+data[0]+' :'+data[1])
+    def join(self, channel):
+        self.go('JOIN '+channel)
+    def part(self, channel, message=''):
+        self.go('PART %s :%s'%(channel,message))
+    def kick(self,nick,channel,message=''):
+        self.go('KICK %s %s :%s!'%(channel,nick,message))
+    def nick(self,nick):
+        self.go('NICK %s'%nick)
+        mynick=nick[:]
+    def notice(self,data):
+        self.go('NOTICE '+data[0]+' :'+data[1])
+    def mode(self,nick,channel,mode):
+        self.go('MODE '+channel+' '+mode+' '+nick)
+    def kill(self,nick,reason=''):#Must be IRCOP
+        self.go('KILL %s :%s' % (nick,reason))
+    def kline(self,host,time='3600',reason='K-Lined'):#Must be IRCOP
+        self.go('KLINE %s %s :%s'%(host,str(time),reason))
+    def raw(self,data):
+        self.go(data)
+    def go(self,data):
+        self.push(data+'\r\n')
+class connection(asynchat.async_chat,queue_class):
     def __init__(self):
         asynchat.async_chat.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,41 +88,8 @@ class connection(asynchat.async_chat):
             continuepgm=0
         for handler in bbot.lhandlers:
             handler.loop()
-        queue.send()
     def collect_incoming_data(self,data):
         self.data+=data
-class queue_class():
-    def __init__(self):
-        self.queue=[]
-        self.conn=connection()
-    def send(self):
-        for each in self.queue:
-            self.conn.push(each+'\r\n')
-        self.queue=[]
-    def get_length(self):
-        return len(self.queue)
-    def append(self,data):
-        self.queue.append('PRIVMSG '+data[0]+' :'+data[1])
-    def pop(self):
-        return self.queue.pop(0)
-    def join(self, channel):
-        self.queue.append('JOIN '+channel)
-    def part(self, channel, message=''):
-        self.queue.append('PART %s :%s'%(channel,message))
-    def kick(self,nick,channel,message=''):
-        self.queue.append('KICK %s %s :%s!'%(channel,nick,message))
-    def nick(self,nick):
-        self.queue.append('NICK %s'%nick)
-        mynick=nick[:]
-    def notice(self,data):
-        self.queue.append('NOTICE '+data[0]+' :'+data[1])
-    def mode(self,nick,channel,mode):
-        self.queue.append('MODE '+channel+' '+mode+' '+nick)
-    def kill(self,nick,reason=''):#Must be IRCOP
-        self.queue.append('KILL %s :%s' % (nick,reason))
-    def kline(self,host,time='3600',reason='K-Lined'):#Must be IRCOP
-        self.queue.append('KLINE %s %s :%s'%(host,str(time),reason))
-    def raw(self,data):
-        self.queue.append(data)
-queue=queue_class() 
+
+queue=connection()
 
