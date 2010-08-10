2dc5696a0cb5edf9e6381270b3f790e708cf73c6
Austin H <aj00200@cube.(none)>
2010-08-10 22:44:49 +0200

    BBot now has the ?version command

--- modules/BBot.py
+++ modules/BBot.py
@@ -97,6 +97,8 @@ class bbot(api.module):
 				if words.lower().find(config.mynick.lower())!=-1 or words.lower()=='aj00200':
 					words=nick
 				self.append((channel,'\x01ACTION kicks %s\x01'%words))
+			elif data.find(':?version'):
+				self.append((channel,'I am version %s.'%BBot.version))
 	def add_factoid(self,query):
 		self.static[query[0].lower()]=query[1]
 	def del_factoid(self,query):
