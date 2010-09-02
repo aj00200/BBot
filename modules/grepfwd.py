f8c748c7401fd2f8f1b19668a933d14fc463a16a
Austin H <aj00200@cube.(none)>
2010-09-02 21:05:10 +0200

    Added grepfwd to BBot

---
+++ modules/grepfwd.py
@@ -0,0 +1,21 @@
+import q
+import re
+import api
+
+class grepfwd(api.module):
+    commands=['grepfwd']
+    def __init__(self,server):
+        self.fwds={}
+        api.module.__init__(self,server)
+    def go(self,nick,data,channel):
+        if ':?grepfwd' in data and ' > ' in data and api.checkIfSuperUser(data):
+            self.add_fwd(data)
+        if ':?grepfwd help' in data:
+            self.append((channel,'%s: The ?grepfwd command allows you to add a regular expression, which, when found in a messsage, will cause the message to be passed on. Syntax: ?grepfwd <expression> > <nick or chan>'%nick))
+        for each in self.fwds:
+            if re.search(each,data):
+                self.append((self.fwds[each],data))
+    def add_fwd(self,data):
+        query=data[data.find(':?grepfwd ')+10:].split(' > ')
+        self.fwds[query[0]]=query[1]
+module=grepfwd
