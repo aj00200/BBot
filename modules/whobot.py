a70f1a050149e674281efc80733d28a4d3185311
AJ00200 <aj0020020@live.com>
2010-10-10 11:45:44 -0500

    Submitted possible patch to WhoBot (updated to use lastest API

--- modules/whobot.py
+++ modules/whobot.py
@@ -8,14 +8,15 @@ class WhoBot(api.module):
                 print nick
                 self.raw('WHOIS %s'%nick)
     def code(self,code,data):
-        if code=='311':
-            self.data=data.split(mynick)[-1].split()
-            self.h=self.data[2]
-            print 'HOST %s'%self.h
-            if self.h.find('webchat/')!=-1:
-                self.ident='!'.join(self.data[1:2])
-                print 'IDENT: %s'%self.ident
-                self.append(('operserv','AKILL ADD !T 6400 %s@%s Spam is offtopic on FOSSnet. Email kline@fossnet.info for help'%(self.ident,self.h)))
-            else:
-                self.append(('operserv','AKILL ADD !T 6400 *!*@'+self.h+' Spam is offtopic on FOSSnet. Email kline@fossnet.info for help.'))
+        if code=='CODE'
+            if data[0]=='311':
+                self.data=data[1].split(mynick)[-1].split()
+                self.h=self.data[2]
+                print 'HOST %s'%self.h
+                if self.h.find('webchat/')!=-1:
+                    self.ident='!'.join(self.data[1:2])
+                    print 'IDENT: %s'%self.ident
+                    self.append(('operserv','AKILL ADD !T 6400 %s@%s Spam is offtopic on FOSSnet. Email kline@fossnet.info for help'%(self.ident,self.h)))
+                else:
+                    self.append(('operserv','AKILL ADD !T 6400 *!*@'+self.h+' Spam is offtopic on FOSSnet. Email kline@fossnet.info for help.'))
 module=WhoBot
