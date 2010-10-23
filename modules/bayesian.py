f1d2fc437ecfdfdba055968e56d7bc0f6f7e4a21
AJ00200 <aj0020020@live.com>
2010-10-22 21:51:13 -0500

    Created beta Beta Bayesian Inference filter

---
+++ modules/bayesian.py
@@ -0,0 +1,53 @@
+import q,api,config,sqlite3
+class module(api.module):
+    def __init__(self,server):
+        self.db=sqlite3.connect('wordstats.sqlite')
+        self.c=self.db.cursor()
+        self.c.execute('''create table if not exists stats (word text, spam, legit)''')
+        self.db.commit()
+        
+        self.ratio=0.01
+        self.damp=0.5
+        api.module.__init__(self,server)
+    def go(self,nick,data,channel):
+        if api.checkIfSuperUser(data):
+            if '?spam ' in data:
+                self.spam(data[data.find('spam ')+5:].lower())
+            elif '?legit ' in data:
+                self.legit(data[data.find('legit ')+6:].lower())
+            elif '?check ' in data:
+                self.check(data[data.find('check ')+6:].lower())
+            elif '?zzz ' in data:
+                self.zzz(data[data.find('zzz ')+4:].lower())
+            elif '?combe' in data:
+                self.db.commit()
+    def spam(self,data):
+        for word in data.split():
+            tmp=self.c.execute('''select * from stats where word=?''',(word,)).fetchall()
+            print(tmp)
+            if len(tmp)>0:
+                self.c.execute('''delete from stats where word=?''',(word,))
+                self.c.execute('''insert into stats values (?, ?, ?)''',(word,tmp[0][1]+1,tmp[0][2]))
+            else:
+                self.c.execute('''insert into stats values (?, ?, ?)''',(word,2,1))
+    def legit(self,data):
+        for word in data.split():
+            tmp=self.c.execute('''select * from stats where word=?''',(word,)).fetchall()
+            print(tmp)
+            if len(tmp)>0:
+                self.c.execute('''delete from stats where word=?''',(word,))
+                self.c.execute('''insert into stats values (?, ?, ?)''',(word,tmp[0][1],tmp[0][2]+1))
+            else:
+                self.c.execute('''insert into stats values (?, ?, ?)''',(word,1,2))
+    def zzz(self,data):
+        print('---zzz data: %s'%data)
+        for word in data.split():
+            tmp=self.c.execute('''select * from stats where word=?''',(word,)).fetchall()
+            self.append((config.error_chan,str(tmp)))
+    def check(self,data):
+        total=0
+        for word in data.split():
+            tmp=self.c.execute('''select * from stats where word=?''',(word,)).fetchall()
+            if len(tmp)>0:
+                total+=((float(tmp[0][1])/float(tmp[0][2]))*self.ratio)/self.damp
+        self.append(('#spam','Level: %s'%total))
