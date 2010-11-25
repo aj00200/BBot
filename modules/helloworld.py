377cdd23a0423b559c747f5049610a466481b1f6
Jason Gates <bikcmp@bikcmp.com>
2010-11-25 13:34:33 -0500

    Added example module helloworld.py

---
+++ modules/helloworld.py
@@ -0,0 +1,10 @@
+import q
+import api
+class module(api.module):
+    def privmsg(self,nick,data,channel): #catch privmsgs
+	  try: #if it can't split the message, ignore the error
+		message = data.split("PRIVMSG "+channel+" :")[1]
+		if message.lower() == "hello":
+			self.msg(channel, "Why, hello there "+nick) #say something to the channel
+	  except: #if I can't split up the message, or something goes wrong, don't do anything
+		  pass 
