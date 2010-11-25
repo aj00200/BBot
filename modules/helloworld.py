b48bdfed36ea5d1ad69e45387a66b49f942e9fde
Jason Gates <bikcmp@bikcmp.com>
2010-11-25 14:25:19 -0500

    Fixed helloworld.py's indentation

--- modules/helloworld.py
+++ modules/helloworld.py
@@ -2,13 +2,13 @@ import q
 import api
 class module(api.module):
     def privmsg(self,nick,data,channel): #catch privmsgs
-	  try: #if it can't split the message, ignore the error
-		message = data[data.find(' :')+2:]
-		if message.lower() == "hello":
-			self.msg(channel, "Why, hello there "+nick) #say something to the channel
-			self.raw("PRIVMSG "+channel+" :I said something via self.raw!") #send something via api.raw()
-			
-	  except: #if I can't split up the message, or something goes wrong, don't do anything
-		  pass 
+      try: #if it can't split the message, ignore the error
+        message = data[data.find(' :')+2:]
+        if message.lower() == "hello":
+            self.msg(channel, "Why, hello there "+nick) #say something to the channel
+            self.raw("PRIVMSG "+channel+" :I said something via self.raw!") #send something via api.raw()
+            
+      except: #if I can't split up the message, or something goes wrong, don't do anything
+          pass 
     def join(self,nick,user,host,channel): #Get joins from async.py
-		self.msg(channel, "NICK: "+nick+" USER: "+user+" HOST: "+host)
+        self.msg(channel, "NICK: "+nick+" USER: "+user+" HOST: "+host)
