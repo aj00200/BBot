b79ea367c0abe30a5d6d67d59f468e2764355e2f
Austin H <aj00200@cube.(none)>
2010-11-25 12:40:47 +0100

    Fixed bikcmp's ugly module to be faster

--- modules/helloworld.py
+++ modules/helloworld.py
@@ -2,9 +2,7 @@ import q
 import api
 class module(api.module):
     def privmsg(self,nick,data,channel): #catch privmsgs
-	  try: #if it can't split the message, ignore the error
-		message = data.split("PRIVMSG "+channel+" :")[1]
-		if message.lower() == "hello":
-			self.msg(channel, "Why, hello there "+nick) #say something to the channel
-	  except: #if I can't split up the message, or something goes wrong, don't do anything
-		  pass 
+	message = data[data.find(' :')+2:]
+	if message.lower() == "hello":
+		self.msg(channel, "Why, hello there "+nick) #say something to the channel
+
