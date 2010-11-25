0aa0de70da4a3b2f94d166e00a12acac2cc34e7f
Jason Gates <bikcmp@bikcmp.com>
2010-11-25 13:15:55 -0500

    fixed small typo in bbot.py

--- bbot.py
+++ bbot.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 #This Bot is licensed under the GNU GPL v3
 
-versio='6.0.0'
+version='6.0.0'
 import socket,asyncore
 if __name__=='__main__':
     import backends.async as backend
