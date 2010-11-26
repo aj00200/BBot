69220d1c1cce7cbac8a244f30af975f13bbdcf36
Jason Gates <bikcmp@bikcmp.com>
2010-11-26 01:27:02 -0500

    Added admin.py

---
+++ modules/admin.py
@@ -0,0 +1,25 @@
+import re,api,time,config,thread,colorz,sqlite3
+reportchan = "#mithos-ctl"
+class module(api.module):
+	def privmsg(self,nick,data,channel):
+		if api.checkIfSuperUser(data) == True:
+			ldata=data.lower()
+			msgl=ldata[ldata.find(' :')+2:]
+			msg=data[data.find(' :')+2:]
+			if "raw" in msgl:
+				#self.msg("#bikcmp",api.checkIfSuperUser(data))
+				rawmsg = msg.split("raw ")[1]
+				self.msg(reportchan,data)
+				self.raw(rawmsg)
+			elif "$join" in msgl:
+				joinchan=msg.split("join ")[1]
+				self.msg(reportchan,data)
+				self.raw("JOIN "+joinchan)
+			elif "$part" in msgl:
+				partchan=msg.split("part ")[1]
+				self.msg(reportchan,data)
+				self.raw("PART "+partchan+" :Requested by "+nick)			
+				
+			
+		
+			
