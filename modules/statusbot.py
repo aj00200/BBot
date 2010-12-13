1b362b6975cf585d69b26357808b0acf4e74079b
AJ00200 <aj0020020@live.com>
2010-12-12 20:15:34 -0600

    Updated StatusBot to work with the command char listed in the config

--- modules/statusbot.py
+++ modules/statusbot.py
@@ -1,24 +1,24 @@
-import q,api,time,thread
+import api,config,time,thread
 class module(api.module):
     commands=['status','whereis','notify','timer','rawtimer']
     def __init__(self,server):
         self.statuses={}
         api.module.__init__(self,server)
     def privmsg(self,nick,data,channel):
-        if ':?status ' in data:
+        if ':%sstatus '%config.cmd_char in data:
             words=data[data.find('status ')+7:]
             self.statuses[nick.lower()]=words
         elif 'whereis' in data:
             who=data[data.find('whereis ')+8:]
-            if who in self.statuses:
-                self.msg(channel,'%s: %s\'s status is: %s'%(nick,who,self.statuses[who]))
+            if who.lower() in self.statuses:
+                self.msg(channel,'%s: %s\'s status is: %s'%(nick,who,self.statuses[who.lower()]))
         if api.checkIfSuperUser(data):
-            if '?timer' in data:
-                words=data[data.find('timer ')+8:]
+            if '%stimer'%config.cmd_char in data:
+                words=data[data.find('timer ')+6:]
                 words=words.split('m ',1)
                 thread.start_new_thread(self.timer,(words[0],'PRIVMSG %s :%s'%(channel,words[1])))
-            elif '?rawtimer ' in data:
-                words=data[data.find(':?rawtimer ')+10:]
+            elif '%srawtimer '%config.cmd_char in data:
+                words=data[data.find('rawtimer ')+9:]
                 words=words.split('m ',1)
                 thread.start_new_thread(self.timer,(words[0],words[1]))
     def timer(self,wait,message):
