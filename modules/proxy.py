6090005bb6e406ded09e6aed301ed875bfc5b047
AJ00200 <aj0020020@live.com>
2010-12-12 22:03:43 -0600

    Started converting trekbot commands to the faster format used in BBot(), however, the code is untested. Note the functions added at the bottom of the file and the optional param argument which should be present on all functions

--- modules/proxy.py
+++ modules/proxy.py
@@ -1,21 +1,22 @@
 import q
 import api
 class module(api.module):
-    commands=[]
-    chan='#bbot'
-    net1='irc.fossnet.info'
-    net2='irc.freenode.net'
-    def __init__(self,server):
-        api.module.__init__(self,server)
-    def go(self,nick,data,channel):
-        if channel==self.chan:
-            if self.__server__==self.net1:
-                self.to=self.net2
-            elif self.__server__==self.net2:
-                self.to=self.net1
-            else:
-                self.to=self.net1
-            try:
-                q.append(self.to,(channel,'<%s> %s'%(nick,data[data.find(' :')+2:])))
-            except Exception:
-                pass
+	commands=[]
+	chan='#bbot'
+	net1='irc.fossnet.info'
+	net2='irc.freenode.net'
+	def __init__(self,server):
+		self.
+		api.module.__init__(self,server)
+	def privmsg(self,nick,data,channel):
+		if channel==self.chan:
+			if self.__server__==self.net1:
+				self.to=self.net2
+			elif self.__server__==self.net2:
+				self.to=self.net1
+			else:
+				self.to=self.net1
+			try:
+				q.append(self.to,(channel,'<%s> %s'%(nick,data[data.find(' :')+2:])))
+			except Exception:
+				pass
--- modules/trekbot.py
+++ modules/trekbot.py
@@ -1,121 +1,144 @@
 import q,api,config
 class module(api.module):
-    commands=['op','deop','kick','ban','unban','nick','echo','mode','voice','devoice','blacklist','unblacklist','listbl','whitelist','unwhitelist','topic']
-    def __init__(self,server=config.network):
-        self.blacklist=[]
-        self.blconfig=open('trekbot/blacklist','r').readlines()
-        for each in self.blconfig:
-            self.blacklist.append(each.strip('\r\n'))
-        self.whitelist=[]
-        self.wlconfig=open('trekbot/whitelist','r').readlines()
-        for each in self.wlconfig:
-            self.whitelist.append(each.strip('\r\n'))
-        del self.blconfig,self.wlconfig
-        self.proxyscan=api.getConfigBool('trekbot','proxy-scan')
-        api.module.__init__(self,server)
-    def privmsg(self,nick,data,channel):
-        ldata=data.lower()
-        self.superuser=api.checkIfSuperUser(data,config.superusers)
-        if self.superuser:
-            if ':?op' in ldata:
-                if '?op ' in ldata:
-                    nick=ldata[ldata.find('?op')+4:]
-                self.mode(nick,channel,'+o')
-            elif ':?deop' in ldata:
-                if '?deop ' in ldata:
-                    nick=ldata[ldata.find('?deop ')+6:]
-                self.mode(nick,channel,'-o')
-            elif ':?voice' in ldata:
-                if '?voice ' in ldata:
-                    nick=ldata[ldata.find('?voice ')+7:]
-                self.mode(nick,channel,'+v')
-            elif ':?devoice ' in ldata:
-                nick=ldata[ldata.find('?devoice ')+9:]
-                self.mode(nick,channel,'-v')
-            elif ':?quiet ' in ldata:
-                nick=ldata[ldata.find('?quiet ')+7:]
-                self.mode(nick,channel,'+q')
-            elif ':?kick ' in ldata:
-                name=ldata[ldata.find('?kick ')+6:]
-                self.kick(name,channel,'Requested by %s'%nick)
-            elif ldata.find('?rehash')!=-1:
-                self.__init__()
-            #Blacklist
-            elif ':?blacklist ' in data:
-                name=data[data.find('?blacklist ')+11:]
-                if not name in self.blacklist:
-                    self.blacklist.append(name)
-                    self.write_blacklist()
-            elif ':?unblacklist ' in ldata:
-                name=data[data.find('?unblacklist ')+13:]
-                if name in self.blacklist:
-                    self.blacklist.pop(self.blacklist.index(name))
-                    self.write_blacklist()
-                else:
-                    self.msg(nick,'That host is not blacklisted')
-            elif ':?listbl' in ldata:
-                self.msg(nick,str(self.blacklist))
-            elif ':?whitelist ' in ldata:
-                name=data[data.find('?whitelist ')+11:]
-                if not name in self.whitelist:
-                    self.whitelist.append(name)
-                    self.write_blacklist()
-            elif ':?unwhitelistlist ' in ldata:
-                name=data[data.find('?unwhitelist ')+13:]
-                if name in self.blacklist:
-                    self.whitelist.pop(self.blacklist.index(name))
-                    self.write_whitelist()
-                else:
-                    self.msg(nick,'That host is not whitelisted')
-            elif ':?listbl' in ldata:
-                self.msg(nick,str(self.blacklist))
-            elif ':?mode ' in ldata:
-                self.mode('',channel,ldata[ldata.find('?mode ')+6:])
-            elif ':?echo ' in ldata:
-                self.msg(channel,data[ldata.find('?echo ')+6:])
-            elif ':?ban ' in ldata:
-                self.mode(data[data.find('?ban ')+5:],channel,'+b')
-            elif ':?unban ' in ldata:
-                self.mode(data[data.find('?unban ')+7:],channel,'-b')
-            elif ':?topic ' in ldata:
-                self.raw('TOPIC %s :%s'%(channel,data[data.find('?topic ')+7:]))
-            elif ':?nick ' in ldata:
-                self.nick(data[data.find('?nick ')+6:])
-    def write_blacklist(self):
-        self.blconfig=open('trekbot/blacklist','w')
-        for each in self.blacklist:
-            self.blconfig.write(each+'\n')
-    def write_whitelist(self):
-        self.wlconfig=open('trekbot/whitelist','w')
-        for each in self.whitelist:
-            self.wlconfig.write(each+'\n')
-    def get_join(self,nick,channel,ip,user):
-        print "GOT JOIN"
-        
-        if not ip in self.blacklist:
-            if not ip in self.whitelist:
-                if self.proxyscan:
-                    self.scan(ip,channel,nick)
-            else:
-                self.mode(nick,channel,'+v')
-        else:
-            self.kick(nick,channel,'You are on the blacklist, please message a channel op about getting removed from the list')
-    def scan(self,ip,channel,nick):
-        self.scansafe=1
-        try:
-            print('Scanning '+ip)
-            self.nm=nmap.PortScanner()
-            #80, 8080, 1080, 3246
-            self.nm.scan(ip,'808,23,1080,110,29505,8080,3246','-T5')
-            for each in nm.all_hosts():
-                print each+':::'
-                lport = nm[each]['tcp'].keys()
-                print lport
-                if 808 in lport or 23 in lport or 110 in lport or 1080 in lport or 29505 in lport or 80 in lport or 8080 in lports or 3246 in lports:
-                    self.scansafe=0
-                    print 'DRONE'
-            del self.nm
-            if self.scansafe:
-                self.mode(nick,channel,'+v')
-        except:
-            print 'PYTHON NMAP CRASH'
+	commands=['op','deop','kick','ban','unban','nick','echo','mode','voice','devoice','blacklist','unblacklist','listbl','whitelist','unwhitelist','topic']
+	def __init__(self,server=config.network):
+		self.blacklist=[]
+		self.blconfig=open('trekbot/blacklist','r').readlines()
+		for each in self.blconfig:
+			self.blacklist.append(each.strip('\r\n'))
+		self.whitelist=[]
+		self.wlconfig=open('trekbot/whitelist','r').readlines()
+		for each in self.wlconfig:
+			self.whitelist.append(each.strip('\r\n'))
+		del self.blconfig,self.wlconfig
+		self.proxyscan=api.getConfigBool('trekbot','proxy-scan')
+		api.module.__init__(self,server)
+		
+		self.su_funcs={
+			'op':self.op,
+			'deop':self.deop,
+			'voice':self.voice,
+			'devoice':self.devoice,
+			'quiet':self.quiet,
+			'nick':self.nick,
+		}
+	def privmsg(self,nick,data,channel):
+		ldata=data.lower()
+		self.superuser=api.checkIfSuperUser(data,config.superusers)
+		# Superuser Commands
+		if self.superuser:
+			if ' :%s' in data:
+				command=data[data.find(' :%s'%config.cmd_char)+2+len(config.cmd_char):]
+				if ' ' in command:
+					param=command[command.find(' ')+1:]
+					command=command[:command.find(' ')]
+				if command in self.su_funcs:
+					self.sufuncs(nick,channel,param)
+	
+			elif ':?voice' in ldata:
+				if '?voice ' in ldata:
+					nick=ldata[ldata.find('?voice ')+7:]
+				self.mode(nick,channel,'+v')
+			elif ':?devoice ' in ldata:
+				nick=ldata[ldata.find('?devoice ')+9:]
+				self.mode(nick,channel,'-v')
+			elif ':?quiet ' in ldata:
+				nick=ldata[ldata.find('?quiet ')+7:]
+				self.mode(nick,channel,'+q')
+			elif ':?kick ' in ldata:
+				name=ldata[ldata.find('?kick ')+6:]
+				self.kick(name,channel,'Requested by %s'%nick)
+			elif ldata.find('?rehash')!=-1:
+				self.__init__()
+			#Blacklist
+			elif ':?blacklist ' in data:
+				name=data[data.find('?blacklist ')+11:]
+				if not name in self.blacklist:
+					self.blacklist.append(name)
+					self.write_blacklist()
+			elif ':?unblacklist ' in ldata:
+				name=data[data.find('?unblacklist ')+13:]
+				if name in self.blacklist:
+					self.blacklist.pop(self.blacklist.index(name))
+					self.write_blacklist()
+				else:
+					self.msg(nick,'That host is not blacklisted')
+			elif ':?listbl' in ldata:
+				self.msg(nick,str(self.blacklist))
+			elif ':?whitelist ' in ldata:
+				name=data[data.find('?whitelist ')+11:]
+				if not name in self.whitelist:
+					self.whitelist.append(name)
+					self.write_blacklist()
+			elif ':?unwhitelistlist ' in ldata:
+				name=data[data.find('?unwhitelist ')+13:]
+				if name in self.blacklist:
+					self.whitelist.pop(self.blacklist.index(name))
+					self.write_whitelist()
+				else:
+					self.msg(nick,'That host is not whitelisted')
+			elif ':?listbl' in ldata:
+				self.msg(nick,str(self.blacklist))
+			elif ':?mode ' in ldata:
+				self.mode('',channel,ldata[ldata.find('?mode ')+6:])
+			elif ':?echo ' in ldata:
+				self.msg(channel,data[ldata.find('?echo ')+6:])
+			elif ':?ban ' in ldata:
+				self.mode(data[data.find('?ban ')+5:],channel,'+b')
+			elif ':?unban ' in ldata:
+				self.mode(data[data.find('?unban ')+7:],channel,'-b')
+			elif ':?topic ' in ldata:
+				self.raw('TOPIC %s :%s'%(channel,data[data.find('?topic ')+7:]))
+			elif ':?nick ' in ldata:
+				self.nick(data[data.find('?nick ')+6:])
+	def write_blacklist(self):
+		self.blconfig=open('trekbot/blacklist','w')
+		for each in self.blacklist:
+			self.blconfig.write(each+'\n')
+	def write_whitelist(self):
+		self.wlconfig=open('trekbot/whitelist','w')
+		for each in self.whitelist:
+			self.wlconfig.write(each+'\n')
+	def get_join(self,nick,channel,ip,user):
+		print "GOT JOIN"
+		
+		if not ip in self.blacklist:
+			if not ip in self.whitelist:
+				if self.proxyscan:
+					self.scan(ip,channel,nick)
+			else:
+				self.mode(nick,channel,'+v')
+		else:
+			self.kick(nick,channel,'You are on the blacklist, please message a channel op about getting removed from the list')
+	def scan(self,ip,channel,nick):
+		self.scansafe=1
+		try:
+			print('Scanning '+ip)
+			self.nm=nmap.PortScanner()
+			#80, 8080, 1080, 3246
+			self.nm.scan(ip,'808,23,1080,110,29505,8080,3246','-T5')
+			for each in nm.all_hosts():
+				print each+':::'
+				lport = nm[each]['tcp'].keys()
+				print lport
+				if 808 in lport or 23 in lport or 110 in lport or 1080 in lport or 29505 in lport or 80 in lport or 8080 in lports or 3246 in lports:
+					self.scansafe=0
+					print 'DRONE'
+			del self.nm
+			if self.scansafe:
+				self.mode(nick,channel,'+v')
+		except:
+			print 'PYTHON NMAP CRASH'
+	
+	# Superuser Commands
+	def op(self,nick,channel,param=None):
+		if not param:
+			self.mode(nick,channel,'+o')
+		else:
+			self.mode(param,channel,'+o')
+	def deop(self,nick,channel,param=None):
+		if not param:
+			self.mode(nick,channel,'-o')
+		else:
+			self.mode(param,channel,'-o')
+
