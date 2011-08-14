0941bc7aa13edc7e8a7a8c598a3925c7bf0bc266
AJ00200 <aj0020@aj00200.org>
2011-08-14 10:36:10 -0500

    Added missing file

---
+++ libs/colorz.py
@@ -0,0 +1,29 @@
+c_open = '\x1B['
+close = c_open+'m'
+colors = {
+    # fg only
+    'red':';31m',
+    'green':';32m',
+    'white':';37m',
+    'blue':';34m',
+    
+    # fg and bg
+    'redblack':'31;40m',
+    'greenblack':'32;40m',
+    'whiteblack':'37;40m',
+    'blueblack':'34;40m',
+    'magenta':'35;40m',
+    'cayn':'36;40m',
+    'yellow':'33;40m'
+
+}
+def encode(text, color):
+    '''Convert text to a color for terminal printing'''
+    if color in colors:
+        return c_open+colors[color]+text + c_open+'m' + close
+    else:
+        raise ColorError('Color %s does not exist' % color)
+    
+class ColorError(Exception):
+    def __init__(self, msg):
+        self.args.append(msg)
