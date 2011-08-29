bfd1860529dacfa1ed506557097315ce7e14937e
AJ00200 <aj0020@aj00200.org>
2011-08-28 19:46:35 -0500

    Removed the script to preform commits in exchange for a new system where changes are merged into launchpad from github when they are stable.

--- api/__init__.py
+++ api/__init__.py
@@ -53,7 +53,9 @@ def connected_to(server):
 hooks = {}
 su_hooks = {}
 def hook_command(name, callback, server, su = False):
-    '''Hook a command for use by the backend, using this when possible will increase the speed of the bot and your module'''
+    '''Hook a command for use by the backend, using this when possible will
+    increase the speed of the bot and your module
+    '''
     if (server not in hooks):
         return False
     try:
