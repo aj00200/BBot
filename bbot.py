3041f56aefa01eef46e429e05111ff0026086254
The Evil Phoenix (TEP) <trekcaptainusa-tw@ubuntu.com>
2012-03-30 14:18:08 -0400

    Version 7.5.8 - Updated Version Number

--- bbot.py
+++ bbot.py
@@ -3,7 +3,7 @@
 It is licensed under the GNU GPL v3 license as well as the MIT license.
 See the README.md file for more details'''
 
-VERSION = '7.5.7'
+VERSION = '7.5.8'
 import sys
 import asyncore
 
